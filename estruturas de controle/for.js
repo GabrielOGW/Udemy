8b28cacb991a1f541afd388a3cb5a4211ceadbc7
GabrielOGW <gabrielogassawara2001@gmail.com>
2022-10-06 16:23:22 -0300

    Conclusão do capitulos de estruturas de controle

---
+++ estruturas de controle/for.js
@@ -0,0 +1,8 @@
+for(let i = 0; i <=10; i++) {
+  console.log(`i = ${i}`)
+}
+
+const notas = [4.2, 6.9, 7.4, 6.4]
+for (let i = 0; i < notas.length; i++) {
+  console.log(`nota ${i + 1}º Bimestre = ${notas[i]}`)
+}
---
+++ estruturas de controle/if1.js
@@ -0,0 +1,26 @@
+function boaNoticia(nota) {
+  if(nota >= 7) {
+    console.log('Aprovado com ' + nota)
+  }
+}
+boaNoticia(4)
+boaNoticia(7)
+boaNoticia(9)
+
+function seForVerdade(valor) {
+  if(valor) {
+    console.log('É verdade..' + valor)
+  }
+}
+
+seForVerdade()
+seForVerdade(null)
+seForVerdade(undefined)
+seForVerdade(NaN)
+seForVerdade('')
+seForVerdade(0)
+seForVerdade(-1)
+seForVerdade(' ')
+seForVerdade('?')
+seForVerdade([])
+seForVerdade([1, 2])
