f18df9afdac06c481f0499c367ae6acb0286c457
GabrielOGW <gabrielogassawara2001@gmail.com>
2022-10-06 10:10:25 -0300

    Conclusão do capitulo de fundamentos

--- fundamentos/erro.js
+++ fundamentos/erro.js
@@ -1,3 +1,16 @@
-function imprimeNome(params) {
-  
+function tratarErro(e) {
+  throw new Error('...')
 }
+
+function imprimeNome(obj) {
+  try {
+    console.log(obj.name.toUpperCase() + "!!!")
+  } catch (e) {
+    tratarErro(e)
+  } finally {
+    console.log('final')
+  }
+}
+
+const obj = { nome: 'Gabriel'}
+imprimeNome(obj)
